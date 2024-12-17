09e2eed48930e2d381f17ddf310cb8376f817ddf
Kainat Asif <sonukainat10@gmail.com>
2024-12-17 21:34:32 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useEffect } from 'react'
+import Hero from "./components/Hero"
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import About from './components/About'
+import AOS from 'aos'
+import "aos/dist/aos.css"
+
+
+const Home = () => {
+  useEffect (() => {
+    AOS.init({
+      easing: "ease-out-back",
+      duration: 1200,
+      delay: 100,
+      mirror: true,
+      anchorPlacement: "bottom-bottom",
+      offset: 160,
+    });
+    AOS.refresh();
+  }, []);
+  
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      
+    </div>
+  )
+}
+
+export default Home
